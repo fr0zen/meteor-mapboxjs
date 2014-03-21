74e20e5ffe35746ee11979336be77673dc9e2e3b
Alan Meira <alanmeira@gmail.com>
2014-03-21 16:25:36 -0300

    no need to export

--- package.js
+++ package.js
@@ -5,5 +5,4 @@ Package.describe({
 Package.on_use(function (api, where) {
   api.add_files('lib/mapbox.js', ['client']);
   api.add_files('lib/mapbox.css', ['client']);
-  api.export('L', 'client');
 });
